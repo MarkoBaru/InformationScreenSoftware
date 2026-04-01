f8ae515d08a982371fa16e3ff9a35720895f686f
MarkoBaru <marko.barutcu@hotmail.com>
2026-04-01 14:23:39 +0200

    debug: Add announcements debug endpoint

--- src/Backend/InformationScreen.Api/Controllers/AnnouncementsController.cs
+++ src/Backend/InformationScreen.Api/Controllers/AnnouncementsController.cs
@@ -19,4 +19,25 @@ public class AnnouncementsController : ControllerBase
     {
         return Ok(await _service.GetActiveForScreenAsync(screenId));
     }
-}
+
+    [HttpGet("debug")]
+    public async Task<IActionResult> Debug()
+    {
+        var swissTz = TimeZoneInfo.FindSystemTimeZoneById("Europe/Zurich");
+        var swissNow = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, swissTz);
+        var all = await _service.GetAllAsync();
+        return Ok(new
+        {
+            serverUtcNow = DateTime.UtcNow.ToString("o"),
+            serverLocalNow = DateTime.Now.ToString("o"),
+            swissNow = swissNow.ToString("o"),
+            totalAnnouncements = all.Count,
+            announcements = all.Select(a => new
+            {
+                a.Id, a.Title, a.IsActive,
+                activeFrom = a.ActiveFrom?.ToString("o"),
+                activeTo = a.ActiveTo?.ToString("o"),
+                a.ExcludedScreenIds
+            })
+        });
+    }
